21e012ad1a9443569f968a4115852a3de1880bc7
Julian Vitor <julian.vtr@gmail.com>
2024-02-20 09:08:45 -0300

    uid e rele

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -7,32 +7,31 @@ const char* ssid = "ESP32AP";
 const char* password = "senha123";
 WebSocketsServer webSocket = WebSocketsServer(81); // Porta WebSocket
 
+#define RELE_PIN 2
+#define RELE_PIN2 4
 #define PINO_SDA 21
 #define PINO_SCL 22
-#define RELE_PIN 2
-Adafruit_PN532 nfc(PINO_SDA, PINO_SCL);
 
-unsigned long lastRFIDReadTime = 0;
-const unsigned long RFIDReadInterval = 2000; // Intervalo de 2 segundos entre as leituras de RFID
+Adafruit_PN532 nfc(PINO_SDA, PINO_SCL);
+String lastUID = ""; // Variável para armazenar o UID anterior lido
+
+void acionarRele(int pin) {
+  digitalWrite(pin, LOW);
+  delay(20000);
+  digitalWrite(pin, HIGH);
+}
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
   switch(type) {
     case WStype_TEXT:
-      Serial.printf("[%u] Mensagem: %s\n", num, payload);
-
-      // Verifica se a mensagem recebida é "ativar 1"
+      Serial.printf("[%u] Text: %s\n", num, payload);
       if (strcmp((char*)payload, "ativar 1") == 0) {
-        // Ativa o relé
-        digitalWrite(RELE_PIN, LOW);
-        Serial.println("Relé ativado");
-
-        // Espera 2 segundos
-        delay(2000);
-
-        // Desativa o relé
-        digitalWrite(RELE_PIN, HIGH);
-        Serial.println("Relé desativado");
+        acionarRele(RELE_PIN);
+      } else if (strcmp((char*)payload, "ativar 2") == 0) {
+        acionarRele(RELE_PIN2);
       }
+      break;
+    default:
       break;
   }
 }
@@ -40,53 +39,66 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
 void setup() {
   Serial.begin(115200);
 
-  // Cria uma rede WiFi
+  pinMode(RELE_PIN, OUTPUT);
+  pinMode(RELE_PIN2, OUTPUT);
+
+  digitalWrite(RELE_PIN, HIGH);
+  digitalWrite(RELE_PIN2, HIGH);
+
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
   Serial.print("Endereço IP do AP: ");
   Serial.println(IP);
 
-  // Inicia o servidor WebSocket
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
 
-  // Inicia o relé
-  pinMode(RELE_PIN, OUTPUT);
-  digitalWrite(RELE_PIN, HIGH); // Desliga o relé inicialmente
-
-  // Inicia o módulo NFC
   nfc.begin();
   nfc.SAMConfig(); // Configura o PN532 para aguardar por um cartão RFID
 }
 
 void loop() {
-  webSocket.loop(); // Processa eventos WebSocket
+  webSocket.loop();
 
-  // Verifica se é hora de fazer uma nova leitura de RFID
-  unsigned long currentMillis = millis();
-  if (currentMillis - lastRFIDReadTime >= RFIDReadInterval) {
-    // Atualiza o tempo da última leitura de RFID
-    lastRFIDReadTime = currentMillis;
+  // Verifica se há um cartão RFID
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer para armazenar o UID lido
+  uint8_t uidLength;                        // Comprimento do UID (4 ou 7 bytes dependendo do tipo de cartão)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 500);
+  
+  if (success) {
+    // Se um cartão foi encontrado, converte o UID para uma string
+    String currentUID = "";
+    for (uint8_t i=0; i < uidLength; i++) {
+      currentUID += String(uid[i], HEX);
+    }
 
-    // Verifica se há um cartão RFID
-    uint8_t success;
-    uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer para armazenar o UID lido
-    uint8_t uidLength;                        // Comprimento do UID (4 ou 7 bytes dependendo do tipo de cartão)
-    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 500);
-    
-    if (success) {
-      // Se um cartão foi encontrado, converte o UID para uma string
-      String uidString = "";
-      for (uint8_t i=0; i < uidLength; i++) {
-        uidString += String(uid[i], HEX);
-      }
-
-      // Envia o UID para todos os clientes conectados
-      webSocket.broadcastTXT(uidString);
+    // Verifica se o UID atual é diferente do UID anterior ou se é a primeira leitura
+    if (currentUID != lastUID || lastUID == "") {
+      // Envia o UID para todos os clientes conectados via WebSocket
+      webSocket.broadcastTXT(currentUID);
 
       // Imprime o UID no Serial Monitor
       Serial.print("UID do cartão RFID: ");
-      Serial.println(uidString);
+      Serial.println(currentUID);
+
+      // Atualiza o último UID lido
+      lastUID = currentUID;
+    }
+
+    // Aguarda um pouco antes de tentar ler outro cartão
+    delay(1000);
+  } else {
+    // Se não foi encontrado um cartão RFID, verifica se o último UID lido foi diferente de vazio
+    if (lastUID != "") {
+      // Envia uma mensagem indicando que o cartão foi removido para todos os clientes conectados via WebSocket
+      webSocket.broadcastTXT("cartao_removido");
+
+      // Imprime no Serial Monitor que o cartão foi removido
+      Serial.println("Cartão RFID removido");
+
+      // Limpa o último UID lido
+      lastUID = "";
     }
   }
 }
